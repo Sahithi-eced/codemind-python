bad4840c245a2f2f437dfb20819a4d4db2dfe583
sahithi <84696192+Sahithi-eced@users.noreply.github.com>
2022-11-02 20:33:48 +0530

    n=int(input()) m=int(input()) for i in range(n,m+1):     if i==0 or i==1:         continue     elif i>1:         for j in range(2,i):             if i%j==0:                 break         else:             print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,11 @@
+n=int(input())
+m=int(input())
+for i in range(n,m+1):
+    if i==0 or i==1:
+        continue
+    elif i>1:
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i)
